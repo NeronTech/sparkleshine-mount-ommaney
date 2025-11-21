c8b18725117cdcbd4700e09564761f470dc7e8b1
= <=>
2025-11-22 05:12:51 +0800

    updated with domains

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -4,34 +4,34 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://yourdomain.com/",
+      url: "https://sparkleshine-mount-ommaney.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1.0,
     },
-    {
-      url: "https://yourdomain.com/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://yourdomain.com/services",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://yourdomain.com/contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://yourdomain.com/dashboard",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
+    // {
+    //   url: "https://nerontech.github.io/sparkleshine-mount-ommaney/",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
+    // {
+    //   url: "https://nerontech.github.io/sparkleshine-mount-ommaney/",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
+    // {
+    //   url: "https://nerontech.github.io/sparkleshine-mount-ommaney/",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
+    // {
+    //   url: "https://nerontech.github.io/sparkleshine-mount-ommaney/",
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 0.6,
+    // },
   ];
 }
