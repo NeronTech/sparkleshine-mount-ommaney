e59ca677a15ba663e4a807f36e56d89e7337abdb
= <=>
2025-11-22 05:41:38 +0800

    update nextconfigjs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
   experimental: {
     appDir: true, // App Router
   },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
+        ],
+      },
+      {
+        source: '/sitemap-:index.xml',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
+        ],
+      },
+      {
+        source: '/robots.txt',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
